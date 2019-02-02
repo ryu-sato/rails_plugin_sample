10331725950b059e134a20b08c1ddcf5b362f7a4
Ryu Sato <ryu@weseek.co.jp>
2019-02-03 02:07:32 +0900

    インスタンスメソッドsquawkを追加

--- lib/rails_plugin_sample/act_as_rails_sample.rb
+++ lib/rails_plugin_sample/act_as_rails_sample.rb
@@ -2,6 +2,12 @@ module RailsPluginSample
   module ActsAsRailsPluginSample
     extend ActiveSupport::Concern
 
+    included do
+      def squawk(string)
+        write_attribute(self.class.rails_plugin_sample_text_field, string.to_squawk)
+      end
+    end
+
     class_methods do
       def acts_as_rails_plugin_sample(options = {})
         cattr_accessor :rails_plugin_sample_text_field, default: (options[:rails_plugin_sample_text_field] || :last_squawk).to_s
